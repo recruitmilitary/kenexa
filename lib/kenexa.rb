12686ef50c326df79c3474e526081c4b7c75b95d
Michael Guterl <mguterl@gmail.com>
2011-04-13 18:12:54 -0400

    move JobCollectionProxy to it's own file

--- lib/kenexa.rb
+++ lib/kenexa.rb
@@ -5,3 +5,4 @@ end
 require 'kenexa/client'
 require 'kenexa/job'
 require 'kenexa/job_detail_parser'
+require 'kenexa/job_collection_proxy'
--- lib/kenexa/client.rb
+++ lib/kenexa/client.rb
@@ -13,66 +13,8 @@ module Kenexa
 
     TEMPLATE_PATH = File.dirname(__FILE__) + "/templates"
 
-    QUESTION_MAP = {
-      :title        => 7996,
-      :city         => 15615,
-      :state        => 15616,
-      :internal_id  => 7972,
-    }.freeze
-
     def initialize(endpoint = DEFAULT_ENDPOINT)
       @uri = URI.parse(endpoint)
-    end
-
-    class JobCollectionProxy
-      instance_methods.each { |m| undef_method m unless m =~ /(^__|^send$|^object_id$)/ }
-
-      def initialize(envelope)
-        @envelope = envelope
-        @jobs = envelope.search("//Job").map { |node|
-          attributes = {
-            :url          => extract_text(node, ".//JobDetailLink"),
-            :last_updated => extract_date(node, ".//LastUpdated"),
-          }
-
-          QUESTION_MAP.keys.each do |attribute|
-            attributes[attribute] = extract_question(node, attribute)
-          end
-
-          Job.new(attributes)
-        }
-      end
-
-      def total
-        @total ||= @envelope.at("//OtherInformation/TotalRecordsFound").text.to_i
-      end
-
-      def max_pages
-        @max_pages ||= @envelope.at("//OtherInformation/MaxPages").text.to_i
-      end
-
-      def method_missing(meth, *args, &block)
-        @jobs.send(meth, *args, &block)
-      end
-
-      private
-
-      def extract_date(node, name)
-        Date.parse extract_text(node, name)
-      end
-
-      def extract_question(node, name)
-        if question_id = QUESTION_MAP[name]
-          extract_text(node, ".//Question[@Id='#{question_id}']")
-        else
-          raise ArgumentError, "missing question mapping for #{name}"
-        end
-      end
-
-      def extract_text(node, xpath)
-        node.at(xpath).text.strip
-      end
-
     end
 
     def jobs(options = {})
