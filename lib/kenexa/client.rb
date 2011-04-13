3229e03e233143ee5e652cf2faee7fc2355f4e47
Michael Guterl <mguterl@gmail.com>
2011-04-13 16:29:15 -0400

    move request XML to template for easier modification

--- lib/kenexa/client.rb
+++ lib/kenexa/client.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'open-uri'
+require 'erb'
 require 'nokogiri'
 
 module Kenexa
@@ -8,63 +9,16 @@ module Kenexa
   class Client
 
     DEFAULT_ENDPOINT = 'http://import.brassring.com/WebRouter/WebRouter.asmx/route'
+    TEMPLATE_PATH = File.dirname(__FILE__) + "/templates"
 
     def initialize(endpoint = DEFAULT_ENDPOINT)
       @uri = URI.parse(endpoint)
     end
 
     def jobs
-      inputXml = <<-EOF
-<Envelope version="01.00">
-  <Sender>
-    <Id>12345</Id>
-    <Credential>25152</Credential>
-  </Sender>
-  <TransactInfo transactId="1" transactType="data">
-    <TransactId>01/27/2010</TransactId>
-    <TimeStamp>12:00:00 AM</TimeStamp>
-  </TransactInfo>
-  <Unit UnitProcessor="SearchAPI">
-    <Packet>
-      <PacketInfo packetType="data">
-        <packetId>1</packetId>
-      </PacketInfo>
-      <Payload>
-        <InputString>
-          <ClientId>25152</ClientId>
-          <SiteId>5244</SiteId>
-          <PageNumber>1</PageNumber>
-          <OutputXMLFormat>0</OutputXMLFormat>
-          <AuthenticationToken/>
-          <HotJobs/>
-          <ProximitySearch>
-            <Distance/>
-            <Measurement/>
-            <Country/>
-            <State/>
-            <City/>
-            <zipCode/>
-          </ProximitySearch>
-          <JobMatchCriteriaText/>
-          <SelectedSearchLocaleId/>
-          <Questions>
-            <Question Sortorder="ASC" Sort="No">
-              <Id>7982</Id>
-              <Value><![CDATA[TG_SEARCH_ALL]]></Value>
-            </Question>
-          </Questions>
-          <Questions>
-            <Question Sortorder="ASC" Sort="No">
-              <Id>15616</Id>
-              <Value><![CDATA[TG_SEARCH_ALL]]></Value>
-            </Question>
-          </Questions>
-        </InputString>
-      </Payload>
-    </Packet>
-  </Unit>
-</Envelope>
-EOF
+      page = 1
+      template = ERB.new(File.read(TEMPLATE_PATH + "/request.erb"))
+      inputXml = template.result(binding)
       response = Net::HTTP.post_form(@uri, "inputXml" => inputXml)
 
       doc = Nokogiri::XML response.body
